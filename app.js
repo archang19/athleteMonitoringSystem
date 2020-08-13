1adad0ac2d8c55a6f31054b067e1b96aea7d3157
Andrew Chang <andrewrchang2001@gmail.com>
2020-08-12 17:54:46 -0700

    cleaned up route organization

--- app.js
+++ app.js
@@ -18,10 +18,8 @@ var Slot = require("./models/slot");
 var Workout = require("./models/workout");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -46,99 +44,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-app.get("/register", function(req, res){
-   res.render("auth/register"); 
-});
-
-app.get("/dashboard", isLoggedIn, function(req, res){
-
-
-  Promise.all([
-  User.find({coaches: { $in: [req.user.username] } }),
-  User.find({athletes: { $in: [req.user.username] } })
-  ])
-  .then(results=>{
-    //results return an array
-
-    const [athletes, coaches] = results;
-
-    console.log("athletes",athletes);
-    console.log("coaches",coaches);
-    res.render("auth/dashboard", {athletes: athletes, coaches: coaches}); 
-  })
-  .catch(err=>{
-    console.error("Something went wrong",err);
-  })
-
-   // User.find({coaches: { $in: [req.user.username] } } , function(err, athletes) {
-   //    console.log(athletes);
-   //    res.render("auth/dashboard", {athletes: athletes}); 
-   // });
-});
-
-app.post("/dashboard", function(req, res) {
-  console.log(req.body.athleteUsername);
-  User.find({username:req.body.athleteUsername}, function(err, athlete) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-         User.updateOne({username: req.user.username}, 
-          {$addToSet: {athletes: req.body.athleteUsername}},
-          function(err2, success) {
-            if (err2) {
-              console.log(err2);
-            }
-            else {
-              console.log(success);
-            }
-        });
-          User.updateOne({username: req.body.athleteUsername}, 
-          {$addToSet: {coaches: req.user.username} },
-          function(err3, success2) {
-            if (err3) {
-              console.log(err3);
-            }
-            else {
-              console.log(success2);
-              res.redirect("/dashboard");
-            }
-        });
-    }
-});
-});
-
-app.post("/register", function(req, res){
-    var isCoach = false;
-    User.register(new User({username: req.body.username, 
-      firstName: req.body.firstName, 
-      lastName: req.body.lastName,}), req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-        }
-        passport.authenticate("local")(req, res, function(){
-           res.redirect("/");
-        });
-    });
-});
-
-app.get("/login", function(req, res){
-   res.render("auth/login"); 
-});
-
-app.post("/login", passport.authenticate("local", {
-    successRedirect: "/dashboard",
-    failureRedirect: "/login"
-}) ,function(req, res){
-});
-
-app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -154,14 +59,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var passport = require("passport");
 var router = express.Router();
+var User = require("../models/user");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,5 +9,95 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.get("/register", function(req, res){
+   res.render("auth/register"); 
+});
+
+router.post("/register", function(req, res){
+    var isCoach = false;
+    User.register(new User({username: req.body.username, 
+      firstName: req.body.firstName, 
+      lastName: req.body.lastName,}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/");
+        });
+    });
+});
+
+router.get("/login", function(req, res){
+   res.render("auth/login"); 
+});
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+router.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+router.get("/dashboard", isLoggedIn, function(req, res){
+  Promise.all([
+  User.find({coaches: { $in: [req.user.username] } }),
+  User.find({athletes: { $in: [req.user.username] } })
+  ])
+  .then(results=>{
+    //results return an array
+
+    const [athletes, coaches] = results;
+
+    console.log("athletes",athletes);
+    console.log("coaches",coaches);
+    res.render("auth/dashboard", {athletes: athletes, coaches: coaches}); 
+  })
+  .catch(err=>{
+    console.error("Something went wrong",err);
+  })
+});
+
+router.post("/dashboard", function(req, res) {
+  console.log(req.body.athleteUsername);
+  User.find({username:req.body.athleteUsername}, function(err, athlete) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+         User.updateOne({username: req.user.username}, 
+          {$addToSet: {athletes: req.body.athleteUsername}},
+          function(err2, success) {
+            if (err2) {
+              console.log(err2);
+            }
+            else {
+              console.log(success);
+            }
+        });
+          User.updateOne({username: req.body.athleteUsername}, 
+          {$addToSet: {coaches: req.user.username} },
+          function(err3, success2) {
+            if (err3) {
+              console.log(err3);
+            }
+            else {
+              console.log(success2);
+              res.redirect("/dashboard");
+            }
+        });
+    }
+});
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
